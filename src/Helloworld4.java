d831dad43e0550c8f4f440cada206d03e91a2285
stellachoi1 <stella.choi@maine.edu>
2019-01-30 13:24:59 -0500

    hi alice

--- src/Helloworld4.java
+++ src/Helloworld4.java
@@ -4,6 +4,7 @@ public class Helloworld4 {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("hi");
+		System.out.println("hi alice");
 		// TODO Auto-generated method stub
 
 	}
